d84ba9afe52670c82e1b837e53538c8e111ce2d8
dasbd72 <twbd723@gmail.com>
2024-07-28 20:28:28 +0800

    feat: added linear layer

---
+++ nn/naive/modules/linear.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+class Linear:
+    def __init__(self, in_features: int, out_features: int):
+        """Creates a Linear layer with in_features input features and out_features output features.
+
+        :param int in_features: number of input features
+        :param int out_features: number of output features
+        """
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = np.random.randn(out_features, in_features)
+        self.bias = np.random.randn(out_features)
+        self.grad_weight = np.zeros_like(self.weight)
+        self.grad_bias = np.zeros_like(self.bias)
+        self.input = None
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        """Performs forward pass through the layer.
+
+        :param np.ndarray x: input tensor of shape (batch_size, in_features)
+        :return: output tensor of shape (batch_size, out_features)
+        :rtype: np.ndarray
+        """
+        return self.forward(x)
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Performs forward pass through the layer.
+
+        :param np.ndarray x: input tensor of shape (batch_size, in_features)
+        :return: output tensor of shape (batch_size, out_features)
+        :rtype: np.ndarray
+        """
+        self.input = x.copy()
+        return np.dot(x, self.weight.T) + self.bias
+
+    def backward(self, grad_output: np.ndarray) -> np.ndarray:
+        """Performs backward pass through the layer.
+
+        :param np.ndarray grad_output: gradient tensor of shape (batch_size, out_features)
+        :return: gradient tensor of shape (batch_size, in_features)
+        :rtype: np.ndarray
+        """
+        self.grad_weight = np.dot(grad_output.T, self.input)
+        self.grad_bias = np.sum(grad_output, axis=0)
+        return np.dot(grad_output, self.weight)
+
+    def update(self, lr: float) -> None:
+        """Updates the weights and biases of the layer.
+
+        :param float lr: learning rate
+        """
+        self.weight -= lr * self.grad_weight
+        self.bias -= lr * self.grad_bias
+
+    def zero_grad(self):
+        """Resets the gradient tensors to zero."""
+        self.grad_weight.fill(0)
+        self.grad_bias.fill(0)
---
+++ nn/naive/modules/linear_test.py
@@ -0,0 +1,108 @@
+import unittest
+
+import numpy as np
+import torch
+
+from .linear import Linear
+
+
+class TestLinear(unittest.TestCase):
+    def test_call(self):
+        w = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        b = np.array([7, 8], dtype=np.float64)
+        x = np.array([[9, 10, 11], [12, 13, 14]], dtype=np.float64)
+
+        layer = Linear(3, 2)
+        layer.weight = w.copy()
+        layer.bias = b.copy()
+        y = layer(x)
+        np.testing.assert_array_almost_equal(y, layer.forward(x), decimal=5)
+
+    def test_forward(self):
+        w = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        b = np.array([7, 8], dtype=np.float64)
+        x = np.array([[9, 10, 11], [12, 13, 14]], dtype=np.float64)
+
+        layer = Linear(3, 2)
+        layer.weight = w.copy()
+        layer.bias = b.copy()
+        y = layer.forward(x)
+
+        pt_layer = torch.nn.Linear(3, 2)
+        pt_layer.weight.data = torch.tensor(w)
+        pt_layer.bias.data = torch.tensor(b)
+        pt_x = torch.tensor(x)
+        pt_y: torch.Tensor = pt_layer(pt_x)
+        np.testing.assert_array_almost_equal(
+            pt_y.detach().numpy(), y, decimal=5
+        )
+
+    def test_backward(self):
+        w = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        b = np.array([7, 8], dtype=np.float64)
+        x = np.array([[9, 10, 11], [12, 13, 14]], dtype=np.float64)
+        grad_output = np.array([[15, 16], [17, 18]], dtype=np.float64)
+
+        layer = Linear(3, 2)
+        layer.weight = w.copy()
+        layer.bias = b.copy()
+        y = layer.forward(x)
+        grad = layer.backward(grad_output)
+
+        pt_layer = torch.nn.Linear(3, 2)
+        pt_layer.weight.data = torch.tensor(w)
+        pt_layer.bias.data = torch.tensor(b)
+        pt_x = torch.tensor(x, requires_grad=True)
+        pt_y: torch.Tensor = pt_layer(pt_x)
+        np.testing.assert_array_almost_equal(
+            pt_y.detach().numpy(), y, decimal=5
+        )
+        pt_y.backward(torch.tensor(grad_output))
+        np.testing.assert_array_almost_equal(pt_x.grad, grad, decimal=5)
+
+    def test_update(self):
+        w = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        b = np.array([7, 8], dtype=np.float64)
+        g_w = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        g_b = np.array([7, 8], dtype=np.float64)
+        lr = 0.5
+
+        layer = Linear(3, 2)
+        layer.weight = w.copy()
+        layer.bias = b.copy()
+        layer.grad_weight = g_w.copy()
+        layer.grad_bias = g_b.copy()
+        layer.update(lr)
+        np.testing.assert_array_almost_equal(layer.weight, w - lr * g_w)
+        np.testing.assert_array_almost_equal(layer.bias, b - lr * g_b)
+
+        pt_layer = torch.nn.Linear(3, 2)
+        pt_layer.weight.data = torch.tensor(w)
+        pt_layer.bias.data = torch.tensor(b)
+        pt_layer.weight.grad = torch.tensor(g_w)
+        pt_layer.bias.grad = torch.tensor(g_b)
+        pt_optim = torch.optim.SGD(pt_layer.parameters(), lr=lr)
+        pt_optim.step()
+        np.testing.assert_array_almost_equal(
+            pt_layer.weight.detach().numpy(), layer.weight, decimal=5
+        )
+        np.testing.assert_array_almost_equal(
+            pt_layer.bias.detach().numpy(), layer.bias, decimal=5
+        )
+
+    def test_zero_grad(self):
+        w = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        b = np.array([7, 8], dtype=np.float64)
+
+        layer = Linear(3, 2)
+        layer.grad_weight = w.copy()
+        layer.grad_bias = b.copy()
+        layer.zero_grad()
+        np.testing.assert_array_almost_equal(
+            layer.grad_weight, np.zeros_like(w)
+        )
+        np.testing.assert_array_almost_equal(layer.grad_bias, np.zeros_like(b))
+
+
+if __name__ == "__main__":
+    unittest.main()
